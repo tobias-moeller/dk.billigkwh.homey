ba8c2f411a66ef5204ad9a2e24e8d06050affd8b
Kim Kokholm <kokholm@gmail.com>
2025-03-14 19:07:59 +0100

    Widget.

--- widgets/graph/api.js
+++ widgets/graph/api.js
@@ -1,51 +1,36 @@
 'use strict';
 
+const _ = require("lodash");
+const moment = require("moment-timezone");
+
 module.exports = {
   async getPrices({ homey, query }) {
     
-    
-    var data = [
-      {
-        hour: 9,
-        price: 2.24
-      },
-      {
-        hour: 10,
-        price: 2.11
-      },
-      {
-        hour: 11,
-        price: 3.40
-      },
-      {
-        hour: 12,
-        price: 2.95
-      },
-      {
-        hour: 13,
-        price: 4.05
-      },
-      {
-        hour: 14,
-        price: 2.01
-      },
-      {
-        hour: 15,
-        price: 4.22
-      }
+    var nullValue = [{ hour: 0, price: 0 }];
 
+    if (homey.drivers.getDriver('dk-energy').getDevices().length == 0){
+      return nullValue;
+    };
 
-    ];
-    
-    // you can access query parameters like "/?foo=bar" through `query.foo`
+    try {
+      var device = homey.drivers.getDriver('dk-energy').getDevices()[0];
+      
+      var pricesForNextHours = await device.getMeterPricesForNextHours();
+      
+      var currentHour = (await device.getDanishDate()).getHours();
+      var prices = _.chain(pricesForNextHours)
+                    .map((price, index) => { return { 
+                                hour: String(((currentHour + index) % 24)).padStart(2, '0'), 
+                                price: price.toFixed(2) }
+                    })
+                    .take(6)
+                    .value()
+                    ;
+      ;
 
-    // you can access the App instance through homey.app
-    // const result = await homey.app.getSomething();
-    // return result;
-
-    // perform other logic like mapping result data
-
-    return data;
-  },
-
+      return prices;
+    } catch (e){
+      return nullValue;
+    }
+  }
 };
